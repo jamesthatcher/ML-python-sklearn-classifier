56db8f6a3f1cbaac1cad05b89ce0faa0d9f42d4d
James Thatcher <james.e.thatcher@googlemail.com>
2019-09-23 21:45:20 +0100

    feat: added example SVM classifier which persists with joblib

--- app.py
+++ app.py
@@ -1,1 +1,38 @@
 # coding=utf-8
+import json
+import os
+
+import sklearn.datasets
+from joblib import dump
+from sklearn import svm
+from sklearn.metrics import f1_score
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+# instantiate classifier and scaler
+clf = svm.SVC(verbose=True)
+scaler = StandardScaler()
+
+# get training data
+X, y = sklearn.datasets.load_iris(True)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y)
+
+# scale data
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+# train model
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+
+# evaluate the quality of the trained model
+f1_metric = f1_score(y_test, y_pred, average='weighted')
+
+# persist model
+dump(clf, 'model.joblib')
+
+# write metrics
+if not os.path.exists("metrics"):
+    os.mkdir("metrics")
+with open("metrics/f1.metric", "w+") as f:
+    json.dump(f1_metric, f)
